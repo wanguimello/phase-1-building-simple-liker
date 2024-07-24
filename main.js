55856c07439652f140a0ce74f3e2f8722f8c9462
joycewangui <joycewangui590@gmail.com>
2024-07-24 18:56:06 +0700

    Adds javascript code

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+const MODAL = document.querySelector('div#modal h2')
+MODAL.classList.add('hidden')
+const MODAL_MESSAGE = document.querySelector('#modal')
+MODAL_MESSAGE.classList.add('hidden')
+MODAL_MESSAGE.style.backgroundColor = 'white'
+
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function() {
+  let likeGlyphs = document.querySelectorAll('.like-glyph')
+  likeGlyphs.forEach(likeGlyph => {
+    likeGlyph.addEventListener('click', function() {
+      mimicServerCall().then(() => {
+        likeGlyph.innerHTML = FULL_HEART
+      }).catch(() => {
+        likeGlyph.innerHTML = EMPTY_HEART
+      })
+    })
+  })
+  
+})
 
 
 
